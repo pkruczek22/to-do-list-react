18c423884e30e4757dc8a6177515f7f06f72ee44
Pawel Kruczek <pkruczek00@gmail.com>
2025-11-15 21:37:51 +0100

    set focus on input

--- src/TaskForm/index.js
+++ src/TaskForm/index.js
@@ -1,25 +1,29 @@
-import {useState} from 'react';
+import { useState, useRef } from 'react';
 import { Form, Input, Button } from './styled';
 
-const TaskForm = ({addNewTask}) => {
+const TaskForm = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null)
+
 
   const onFormSubmit = (event) => {
     event.preventDefault();
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
+    inputRef.current.focus();
   };
 
   return (
-  <Form onSubmit={onFormSubmit}>
-    <Input 
-      value={newTaskContent}
-      onChange={({target}) => setNewTaskContent(target.value)}
-      type="text"
-      placeholder="Co jest do zrobienia?"
-    />
-    <Button>Dodaj zadanie</Button>
-  </Form>
+    <Form onSubmit={onFormSubmit}>
+      <Input
+        value={newTaskContent}
+        onChange={({ target }) => setNewTaskContent(target.value)}
+        type="text"
+        placeholder="Co jest do zrobienia?"
+        ref={inputRef}
+      />
+      <Button>Dodaj zadanie</Button>
+    </Form>
   );
 };
 
